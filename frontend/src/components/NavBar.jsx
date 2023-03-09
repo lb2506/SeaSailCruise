82a7f1ed2ecb2b9932260b3a9beb45681a847213
Léo Baccialone <leo.baccialone@globalservs.com>
2023-03-09 17:59:34 +0100

    mobile en cours

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -14,41 +14,31 @@ const NavBar = () => {
 
   return (
     <div className="navbar-container">
-      <nav className="nav-bar">
-        <Link to="/" className="logo-ctn">
+      <nav className="navbar">
+        <Link to="/" className="navbar__logo">
           <img className="logo" src={logo} alt="logo" />
         </Link>
-
+        <div className="navbar__menu">
+          <span className="navbar__menu-burger" />
+        </div>
         {auth._id ? (
-          <Links>
-            <div>
+          <div className="navbar__items">
+            <div className="navbar__items-link">
               <Link to="/cart">
-                <div className="nav-bag">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="35"
-                    height="35"
-                    fill="currentColor"
-                    className="bi bi-handbag-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 1a2 2 0 0 0-2 2v2H5V3a3 3 0 1 1 6 0v2h-1V3a2 2 0 0 0-2-2zM5 5H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11v1.5a.5.5 0 0 1-1 0V5H6v1.5a.5.5 0 0 1-1 0V5z" />
-                  </svg>
-                  <span className="bag-quantity">
-                    <span>{cartItems.length}</span>
-                  </span>
-                </div>
+                <span className="bag-quantity">
+                  <span>{cartItems.length}</span>
+                </span>
               </Link>
             </div>
-            <div>
+            <div className="navbar__items-link">
               <Link to={`/account/orders/${auth._id}`}>Mon compte</Link>
             </div>
             {auth.isAdmin ? (
-              <div>
+              <div className="navbar__items-link">
                 <Link to="/admin/summary">Dashboard</Link>
               </div>
             ) : null}
-            <div
+            <div className="navbar__items-link"
               onClick={() => {
                 dispatch(logoutUser(null));
                 navigate("/");
@@ -57,7 +47,7 @@ const NavBar = () => {
             >
               Déconnexion
             </div>
-          </Links>
+          </div>
         ) : (
           <AuthLinks>
             <Link to="/login">Connexion</Link>
@@ -76,17 +66,5 @@ const AuthLinks = styled.div`
     &:last-child {
       margin-left: 2rem;
     }
-  }
-`;
-
-const Links = styled.div`
-  color: white;
-  display: flex;
-
-  div {
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    margin-left: 2rem;
   }
 `;
